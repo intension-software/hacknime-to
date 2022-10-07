2c54fea0545abf4829abd5bcea58d9e56ed253f8
Svaagi <jozefsvagerkom5@gmail.com>
2022-10-07 20:00:06 +0200

    PWA

---
+++ pages/register.tsx
@@ -0,0 +1,14 @@
+import type { NextPage } from 'next'
+import Image from 'next/image'
+import styles from '../styles/Register.module.css'
+import logo from '../public/images/logo.png'
+
+const Register: NextPage = () => {
+  return (
+    <div>
+      <Image src={logo} alt="logo of InfoMentor" />
+    </div>
+  )
+}
+
+export default Register
