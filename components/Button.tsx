9954e41e956e6c9596fb93ff06e05e433a76afda
Marko Tomčík <markotomcik@gmail.com>
2022-10-08 02:26:38 +0200

    funkcny register ig

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,12 +1,17 @@
 interface ButtonProps {
   children: string
-
+  onClick?: () => void
+  disabled?: boolean
 }
 
-function Button({ children }: ButtonProps) {
+function Button({ children, onClick, disabled }: ButtonProps) {
   return (
     <div className="rounded-full border-4 border-black w-max">
-      <button className="bg-red rounded-full text-white py-1.5 px-5 hover:ring-4 hover:ring-red hover:ring-opacity-50">
+      <button
+        className="bg-red rounded-full text-white py-1.5 px-5 hover:ring-4 hover:ring-red hover:ring-opacity-50"
+        onClick={onClick}
+        disabled={disabled}
+      >
         {children}
       </button>
     </div>
--- pages/register.tsx
+++ pages/register.tsx
@@ -3,8 +3,26 @@ import Image from 'next/image'
 import logo from '../public/images/logoVariant2.svg'
 import Button from '../components/Button'
 import Link from 'next/link'
+import { useState } from 'react'
+import { trpc } from '../utils/trpc'
 
 const Register: NextPage = () => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [password2, setPassword2] = useState('')
+  const [name, setName] = useState('')
+  const [gdpr, setGdpr] = useState(false)
+
+  const mutation = trpc.userCreate.useMutation()
+
+  const handleSubmit = () => {
+    mutation.mutate({
+      email,
+      password,
+      name
+    })
+  }
+
   return (
     <div className="grid h-screen w-screen place-items-center">
       <div className="px-8">
@@ -24,18 +42,28 @@ const Register: NextPage = () => {
             <input
               className="rounded-full bg-black py-1.5 px-5 text-white"
               placeholder="Meno a priezvisko"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
             />
             <input
               className="rounded-full bg-black py-1.5 px-5 text-white"
               placeholder="Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
             />
             <input
               className="rounded-full bg-black py-1.5 px-5 text-white"
               placeholder="Heslo"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
             />
             <input
               className="rounded-full bg-black py-1.5 px-5 text-white"
               placeholder="Zopakujte heslo"
+              type="password"
+              value={password2}
+              onChange={(e) => setPassword2(e.target.value)}
             />
             <label
               htmlFor="gdpr"
@@ -45,12 +73,21 @@ const Register: NextPage = () => {
                 type="checkbox"
                 id="gdpr"
                 className="hidden"
+                checked={gdpr}
+                onChange={(e) => setGdpr(e.target.checked)}
               />
-              <span className="bg-black rounded-full w-6 h-6 inline-block mr-2"></span>
+              <span className={`bg-${gdpr ? 'green' : 'black'} rounded-full w-6 h-6 inline-block mr-2`}></span>
               Súhlasím so <span className="underline">spracovaním osobných údajov</span>
             </label>
           </div>
-          <div className="justify-center flex translate-y-1/2"><Button>Registrovať sa</Button></div>
+          <div className="justify-center flex translate-y-1/2">
+            <Button
+              onClick={handleSubmit}
+              disabled={password !== password2 || !gdpr}
+            >
+              Registrovať sa
+            </Button>
+          </div>
         </div>
         <div className="text-white pt-8">
           Máte účet? <Link href="/login" className="underline">Prihláste sa</Link>
